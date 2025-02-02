752834bf198f8daccb41e1862e452b49d30ba42a
Oscar Ayra <oscar.ayras@gmail.com>
2025-02-02 03:42:51 -0500

    init

--- variables.tf
+++ variables.tf
@@ -1,9 +1,59 @@
-variable "vpc_id" {
-  description = "ID of the VPC"
+# variable "vpc_id" {
+#   description = "ID of the VPC"
+#   type        = string
+# }
+
+# variable "alb_name" {
+#   description = "Names of the Application Load Balancer"
+#   type        = string
+# }
+
+variable "execution_role_arn" {
+  description = "ARN del ECS Execution Role existente"
   type        = string
 }
 
-variable "alb_name" {
-  description = "Names of the Application Load Balancer"
+# variable "cluster_name" {
+#   description = "Nombre del cluster ECS existente"
+#   type        = string
+# }
+
+# variable "private_subnet_ids" {
+#   description = "IDs de las subnets privadas"
+#   type        = list(string)
+# }
+
+# variable "ecs_tasks_security_group_id" {
+#   description = "ID del Security Group existente para las tareas ECS"
+#   type        = string
+# }
+
+# variable "target_group_arn" {
+#   description = "ARN del Target Group existente"
+#   type        = string
+# }
+
+# Variables más concisas y agrupadas
+variable "environment" {
+  description = "Ambiente de despliegue (dev, staging, prod)"
   type        = string
 }
+
+variable "app" {
+  description = "Configuración de la aplicación"
+  type = object({
+    name     = string
+    image    = string
+    port     = number
+    cpu      = number
+    memory   = number
+  })
+  default = {
+    name     = "mi-aplicacion"
+    image    = "mi-imagen:latest"
+    port     = 80
+    cpu      = 256
+    memory   = 512
+  }
+}
+
